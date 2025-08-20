8e80c1d94ce902db941bc98ef5971b8f48109806
Workfoce-prog <ziyackcoulibaly@gmail.com>
2025-08-20 16:35:37 -0500

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import os, sys, streamlit as st, platform
-st.title("Streamlit Debug")
-st.write({"python": sys.version, "platform": platform.platform(), "cwd": os.getcwd()})
-st.write("Files here:", sorted(os.listdir(".")))
